598b3f5d88ce832b74a5cb5f9be80369d9565b2c
Pulkit Bhasin <pulkitb124@berkeley.edu>
2021-07-14 18:00:00 -0700

    Functionality updated

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # FifaTournamentGenerator
+
+## How to run
+
+1. Clone the repo locally by running the follow on your terminal:
+
+```shell 
+git clone https://github.com/pulkitbhasin/FifaTournamentGenerator.git
+```
+
+2. Then run the following to install the only dependency required:
+    ```shell
+    pip install tabulate
+    ```
+
+3. Then run the following to run the Tournament Generator:
+    ```shell
+    python main.py
+    ```
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import _pickle as cPickle
 def createTournament(numPlayers, teamSelectionType):
     playerNames = []
     players = []
-    for i in range(1, int(numPlayers) + 1):
+    for i in range(1, numPlayers + 1):
         playerName = input("Enter name of player " + str(i) + " ")
         playerNames.append(playerName)
     print("")
@@ -23,56 +23,69 @@ def readTournamentFromFile():
 
 if __name__ == '__main__':
     choiceSelected = False
+    print("\nWelcome to the FIFA League Generator!\n")
     while not choiceSelected:
-        choice = input("Welcome to the FIFA League Generator!\n" +
+        choice = input("\n" +
                         "'Create League' - Create a new FIFA league tournament\n" +
                         "'Display Schedule' - Display the remaining matches scheduled\n" +
                         "'Display Week Schedule' - Display matches scheduled for a particular week\n" +
                         "'Update Week Results' - Enter results for this week's matches \n" +
                         "'Display Table' - Display current Points Table\n" + 
+                        "'Delete League' - Delete existing league\n" +
+                        "'Close Generator' - Close this generator script\n" +
+                        "\n"
                         "Enter one of the following options without quotes: ")
         if choice == "Create League":
-            numPlayers = input("Enter number of players participating in league: ")
+            print("")
+            numPlayers = int(input("Enter number of players participating in league: "))
+            while numPlayers%2 == 1:
+                numPlayers = int(input("Number of players participating must be even. Please re-enter number of players participating in league: "))
             createTournament(numPlayers, "Draw")
-            choiceSelected = True
             print("")
         elif choice == "Display Schedule":
             if os.path.isfile('tournament.pickle'):
                 tournament = readTournamentFromFile()
                 tournament.displayFullRemainingSchedule()
-                choiceSelected = True
             else:
                 print("")
-                print("Please create a tournament first")
+                print("Please create a league first")
                 print("")
         elif choice == "Display Week Schedule":
             if os.path.isfile('tournament.pickle'):
                 tournament = readTournamentFromFile()
+                print("")
                 weekNum = input("Enter week number")
+                print("")
                 tournament.displayWeekSchedule(weekNum)
-                choiceSelected = True
             else:
                 print("")
-                print("Please create a tournament first")
+                print("Please create a league first")
                 print("")
         elif choice == "Update Week Results":
             if os.path.isfile('tournament.pickle'):
                 tournament = readTournamentFromFile()
                 tournament.updateWeeklyResults()
-                choiceSelected = True
             else:
                 print("")
-                print("Please create a tournament first")
+                print("Please create a league first")
                 print("")
         elif choice == "Display Table":
             if os.path.isfile('tournament.pickle'):
                 tournament = readTournamentFromFile()
                 tournament.displayTable()
-                choiceSelected = True
             else:
                 print("")
-                print("Please create a tournament first")
+                print("Please create a league first")
                 print("")
+        elif choice == "Delete League":
+            if os.path.isfile('tournament.pickle'):
+                os.remove('tournament.pickle')
+            else:
+                print("")
+                print("No league exists.")
+        elif choice == "Close Generator":
+            choiceSelected = True 
         else:
+            print("")
             print("Invalid option selected. Please try again.")
     
